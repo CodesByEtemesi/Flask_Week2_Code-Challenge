eeb8b863d0f39cd970ec4c54ea1e2220c46b7779
ETEMESI <samweletemesi2@Gmail.com>
2024-04-21 11:52:43 +0300

    complete code challenge

--- server/models.py
+++ server/models.py
@@ -1,8 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
+from sqlalchemy import MetaData, ForeignKey
+from sqlalchemy.orm import validates, relationship
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
+
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -18,6 +19,7 @@ class Sweet(db.Model, SerializerMixin):
     name = db.Column(db.String)
 
     # Add relationship
+    vendor_sweets = relationship('VendorSweet', back_populates='sweet')
     
     # Add serialization
     
@@ -32,7 +34,7 @@ class Vendor(db.Model, SerializerMixin):
     name = db.Column(db.String)
 
     # Add relationship
-    
+    vendor_sweets = relationship('VendorSweet', back_populates='vendor')
     # Add serialization
     
     def __repr__(self):
@@ -46,10 +48,21 @@ class VendorSweet(db.Model, SerializerMixin):
     price = db.Column(db.Integer, nullable=False)
 
     # Add relationships
-    
+    sweet_id = db.Column(db.Integer, ForeignKey('sweets.id'), nullable=False)
+    vendor_id = db.Column(db.Integer, ForeignKey('vendors.id'), nullable=False)
     # Add serialization
-    
+    sweet = relationship('Sweet', back_populates='vendor_sweets')
+    vendor = relationship('Vendor', back_populates='vendor_sweets')
+
     # Add validation
-    
+    @validates('price')
+    def validate_price(self, key, price):
+        if price is None:
+            raise ValueError('Price must have a value.')
+        if price < 0:
+            raise ValueError('Price must be non-negative.')
+        return price
+        
     def __repr__(self):
         return f'<VendorSweet {self.id}>'
+
